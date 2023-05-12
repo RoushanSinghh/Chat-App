ea69fb1a03fd17cc4c2a84b8511d3ad81477165c
Roushan Singh <Roushankr.singh007@gmail.com>
2023-05-12 13:14:48 +0530

    fetching & sharing location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,14 +1,31 @@
 const socket = io()
 
-socket.on('message' ,(message) => {
-    console.log(message );
-    
+socket.on("message", (message) => {
+  console.log(message);
+});
+
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const message = e.target.elements.message.value
+
+  socket.emit("sendMessage", message)
 })
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
-    e.preventDefault()
+document.querySelector("#send-location").addEventListener("click", () => {
+  
+  console.log('ff');
+  
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser.')
+  }
+      
+    navigator.geolocation.getCurrentPosition((position) => {
+     socket.emit('sendLocation', {
+           latitude: position.coords.latitude,
+           longitude: position.coords.longitude
+     })
+    })
 
-    const message = e.target.elements.message.value
+  })
 
-    socket.emit('sendMessage', message)
-})

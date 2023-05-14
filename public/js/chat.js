db8d2248e27c4551ba81c2de5defdb624d0eb3e4
Roushan Singh <Roushankr.singh007@gmail.com>
2023-05-14 12:23:16 +0530

    rendering messages and location

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,10 +7,28 @@ const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 
 const $sendLocationButton = document.querySelector("#send-location")
+const $messages = document.querySelector('#messages')
 
-socket.on("message", (message) => {
+// templates
+
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+
+socket.on('message', (message) => {
   console.log(message);
-});
+  const html = Mustache.render(messageTemplate, {
+    message 
+  })
+  $messages.insertAdjacentHTML('beforeend', html )
+})
+
+socket.on('locationMessage', (url) => {
+  console.log(url)
+  const html = Mustache.render(locationMessageTemplate, {
+    url
+  })
+  $messages.insertAdjacentHTML('beforeend', html)
+})
 
 $messageForm.addEventListener("submit", (e) => {
   e.preventDefault()
